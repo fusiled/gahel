93c223950c7c97bb791ccacea0d51019c9ba1f84
Matteo M. Fusi <fusiled@gmail.com>
2019-06-10 23:11:03 +0200

    docker: added support for multiple terminals for the container and gdb

--- launch.sh
+++ launch.sh
@@ -7,12 +7,15 @@ if [ ! "$(docker images -q $IMAGENAME)" ]; then
 fi
 if [ ! "$(docker ps -a -q -f name=$CONTAINERNAME)" ]; then
     echo "[b1] Creating container $CONTAINERNAME"
-    docker run \
-        -it -v $PWD:/gahel  --name $CONTAINERNAME \
+    docker run --cap-add=SYS_PTRACE  \
+        -it -d -v $PWD:/gahel  --name $CONTAINERNAME \
         $IMAGENAME  /bin/bash /gahel/support/setup_system.sh $USER $(id -u ${USER}) $(id -gn $USER)  $(id -g ${USER})
 else
+    if [ ! "$(docker ps -q -f name=$CONTAINERNAME)" ]; then
         echo "[b2] Restarting existing container named $CONTAINERNAME"
         docker restart $CONTAINERNAME
-        docker attach $CONTAINERNAME
+    fi
 fi
 
+echo "[b2] Restarting existing container named $CONTAINERNAME"
+docker exec -it $CONTAINERNAME bash
