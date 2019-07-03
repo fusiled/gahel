5e07240168456ad2379ea23b1ce55b15b9b1cb2e
Matteo M. Fusi <fusiled@gmail.com>
2019-07-03 17:23:07 +0200

    verilog-includes: added broken division

--- include/verilog/float.v
+++ include/verilog/float.v
@@ -1,2 +1,16 @@
 `include "fadd16.v"
-`include "fmul16.v"
+`include "fmuldiv16.v"
+
+//Just swap the sign bit of A
+module finv16(input [15:0] A, output [15:0] B);
+    assign B = {~A[15],A[14:0]};
+endmodule
+
+
+module fsub(input [15:0] A, input [15:0] B, output[15:0] C);
+    wire [15:0] minusB;
+
+    finv16 invertB(B,minusB);
+    fadd16 adder(A,minusB,C);
+
+endmodule
